cd0b952477ee37569fc6744d491c561086f8c4c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-30 04:38:31 +0000

    Deploy AntoineDao/daylight-artist to github.com/AntoineDao/daylight-artist.git:gh-pages

---
+++ precache-manifest.f443048222c0b7e7ec399fe8abea6299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b5ed609454fe53be9976f15e86a43c",
+    "url": "/daylight-artist/index.html"
+  },
+  {
+    "revision": "4325a2f82be49f83088d",
+    "url": "/daylight-artist/static/css/2.18f57b05.chunk.css"
+  },
+  {
+    "revision": "76478e7d828bc1845129",
+    "url": "/daylight-artist/static/css/main.190967fc.chunk.css"
+  },
+  {
+    "revision": "4325a2f82be49f83088d",
+    "url": "/daylight-artist/static/js/2.5c726de4.chunk.js"
+  },
+  {
+    "revision": "76478e7d828bc1845129",
+    "url": "/daylight-artist/static/js/main.1ea4a5dc.chunk.js"
+  },
+  {
+    "revision": "6387025a1ff8fcecf45c",
+    "url": "/daylight-artist/static/js/runtime~main.496c9ea8.js"
+  }
+]);
